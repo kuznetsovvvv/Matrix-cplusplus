0f16757fa8da9cdf4fbbb106b67479c9aa308ba7
kuznetsovvvv <145388712+kuznetsovvvv@users.noreply.github.com>
2024-07-05 12:46:27 +0300

    Update Cmatrix.h

---
+++ matrix_lib/Cmatrix.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <string>
+#define p 7
+using namespace std;
+class CMatrix {
+private:
+    int rows, colls; // строки, столбцы
+    int** matrix;
+
+public:
+    CMatrix();
+    CMatrix(int rws, int cls);
+    CMatrix(int rws, int cls, int** v);
+    CMatrix(const CMatrix& V);
+    CMatrix(CMatrix&& V) noexcept;
+    ~CMatrix();
+
+    void Del();
+    void print(string n);
+    void ymench();
+    void up();
+
+    int Get_rows();
+    int Get_colls();
+    const void sum1(CMatrix& v);
+    friend CMatrix operator +(CMatrix& v1, CMatrix& v2);
+    CMatrix& operator =(CMatrix&& v) noexcept;
+    CMatrix& operator=(const CMatrix& V);
+};
